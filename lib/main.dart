0011405b7f9fc2d78ddaba2c83e29505ffa7faf3
tsunoda <tsunoda@aomori-u.ac.jp>
2023-02-11 15:55:54 +0900

    石垣さんのプログラム(2023/02/11時点)に置き換え、引数渡しの部分だけ抽出、修正。

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,101 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      title: 'カレンダー',
+      home: FirstPage(),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  late int ID;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('スケジュール共有'),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            child: TextField(decoration: InputDecoration(
+              hintText: 'カレンダーID',
+            ),
+              onChanged: (text) {
+                ID = int.parse(text);
+                print("########## onChanged ID: " + ID.toString());
+              },
+            ),
+          ),
+          Container(
+            child: TextButton(
+              child: Text("カレンダーへ"),
+              onPressed: () async{
+                print("########## onPressed ID: " + ID.toString());
+                await Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => MyHomePage(value: ID)));
+              },
+            ),
+          ),
+          Container(
+
+          ),
+          Container(
+            child: TextButton(
+              child: Text("新規作成"),
+              onPressed: () async{
+
+                print('##########');
+              },
+            ),
+          ),
+        ],
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  final int value;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key, required this.value}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  //createState()でState（Stateを継承したクラス）を返す
+  _MyHomePageState createState() {
+    print("########## MyHomePage value: " + value.toString());
+
+    return _MyHomePageState();
+  }
 }
 
+//Stateをextendsしたクラスを作る
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  late int state = widget.value;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    print("########## MyHomePageState state: " + state.toString());
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        appBar: AppBar(title: Text('スケジュール共有( '/*+ ID +*/')')), body: buildBody(context));
   }
+
+  Widget buildBody(BuildContext context) {
+    return Text("ここ");
+   }
+
 }
